c8b7868e12c0e29f6a46699fbfe7dc19fa95a30a
Marina Ferrari <marinaferrarim@gmail.com>
2025-11-18 12:12:28 -0300

    fixing cors related issues

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,23 @@ const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({origin: 'http://localhost:5173'}))
+const allowedOrigins = [
+    'https://restaurante-cap-frontend.onrender.com/cardapio',
+    'http://localhost:5173'
+]
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+
+app.use(cors({ corsOptions }))
 
 app.get('/api/cardapio', (req, res) => {
     const filePath = path.join(__dirname, 'cardapio', 'cardapio.json');
