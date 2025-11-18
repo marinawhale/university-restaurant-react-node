3dde218f01a12b86b708eb52d7eddcb706feca39
Marina Ferrari <marinaferrarim@gmail.com>
2025-11-18 12:00:50 -0300

    changes in basename

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,8 +6,10 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer'
 import { BrowserRouter } from 'react-router-dom'
 
-const reponame = '/university-restaurant-react-node/';
-const basename = import.meta.env.DEV ? '/' : repoName;
+// const reponame = '/university-restaurant-react-node/';
+// const basename = import.meta.env.DEV ? '/' : reponame;
+
+const basename = '/'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
